b27004a63a87a2a0976a7e0c9fc1ea55f68326b6
Jahongir <jahongirhakimjonov@gmail.com>
2024-02-11 22:12:32 +0500

    commit

--- config/settings.py
+++ config/settings.py
@@ -4,12 +4,11 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-qva^=1k%toyr#73(d&-doudx1uu0-(n((blt^zc7exp&di8-#u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -116,7 +115,6 @@ handler404 = "blog.handlers.handler_404"
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
