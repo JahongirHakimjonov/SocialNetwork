081222447aa57f5acb94bd36acbdfba610c8d648
Jahongir <jahongirhakimjonov@gmail.com>
2024-02-11 20:46:31 +0500

    commit

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qva^=1k%toyr#73(d&-doudx1uu0-(n((blt^zc7exp&di8-#u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
